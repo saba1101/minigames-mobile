d492666e55f6bce0b1958ce74bece42c1cf86e24
saba1101 <55694011+saba1101@users.noreply.github.com>
2021-04-22 18:24:21 +0400

    Update animation.js

--- script/animation.js
+++ script/animation.js
@@ -64,3 +64,31 @@
 // })
 
 
+var ball   = document.querySelector('.mini-games-content-wrapper');
+
+var maxX = $(window).clientWidth  - ball.clientWidth;
+var maxY = $(window).clientHeight - ball.clientHeight;
+
+function handleOrientation(event) {
+  var x = event.beta;  // In degree in the range [-180,180)
+  var y = event.gamma; // In degree in the range [-90,90)
+
+
+  // We constrain the x value to the range [-90,90]
+  if (x >  90) { x =  90};
+  if (x < -90) { x = -90};
+
+  // To make computation easier we shift the range of
+  // x and y to [0,180]
+  x += 90;
+  y += 90;
+
+  // 10 is half the size of the ball
+  // It center the positioning point to the center of the ball
+  ball.style.top  = (maxY*y/180 - 10) + "px";
+  ball.style.left = (maxX*x/180 - 10) + "px";
+}
+
+window.addEventListener('deviceorientation', handleOrientation);
+
+
